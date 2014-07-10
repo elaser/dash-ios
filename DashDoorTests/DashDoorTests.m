717ba16b5f1b11ccbac211c899996a58460ec1df
Anderthan Hsieh <anderthan@gmail.com>
2014-07-09 23:31:57 -0700

    Initial Commit

---
+++ DashDoorTests/DashDoorTests.m
@@ -0,0 +1,34 @@
+//
+//  DashDoorTests.m
+//  DashDoorTests
+//
+//  Created by Anderthan Hsieh on 7/9/14.
+//  Copyright (c) 2014 ahsieh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DashDoorTests : XCTestCase
+
+@end
+
+@implementation DashDoorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
